741f845cee4589ba19976e257b0b263160a8be7b
Chad Rutherford <chad@piso13.com>
2021-02-11 13:30:33 -0500

    Initial Commit

---
+++ YOKE_Take_Home_Chad_Rutherford/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YOKE_Take_Home_Chad_Rutherford
+//
+//  Created by Chad Rutherford on 2/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
