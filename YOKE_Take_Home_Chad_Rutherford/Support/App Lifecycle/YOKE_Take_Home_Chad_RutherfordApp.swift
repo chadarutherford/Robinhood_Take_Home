6971bf55f1b73df6c37d430077d4cec002931fc9
Chad Rutherford <chad_rutherford@outlook.com>
2021-02-15 15:18:24 -0500

    Added Data Controller

--- YOKE_Take_Home_Chad_Rutherford/Support/App Lifecycle/YOKE_Take_Home_Chad_RutherfordApp.swift
+++ YOKE_Take_Home_Chad_Rutherford/Support/App Lifecycle/YOKE_Take_Home_Chad_RutherfordApp.swift
@@ -10,9 +10,27 @@ import SwiftUI
 
 @main
 struct YOKE_Take_Home_Chad_RutherfordApp: App {
+
+    @StateObject var dataController: DataController
+
+    init() {
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
+    }
+
     var body: some Scene {
         WindowGroup {
             MainTabView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
+                .onReceive(
+                    NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification),
+                    perform: save
+                )
         }
+    }
+
+    func save(_ note: Notification) {
+        dataController.save()
     }
 }
---
+++ YOKE_Take_Home_Chad_Rutherford/Support/Controller/DataController.swift
@@ -0,0 +1,59 @@
+//
+//  DataController.swift
+//  YOKE_Take_Home_Chad_Rutherford
+//
+//  Created by Chad Rutherford on 2/15/21.
+//  Copyright © 2021 Chad A. Rutherford. All rights reserved.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "StockRecords", managedObjectModel: Self.model)
+
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Fatal error loading stores: \(error.localizedDescription)")
+            }
+        }
+    }
+
+    static let model: NSManagedObjectModel = {
+        guard let url = Bundle.main.url(forResource: "StockRecords", withExtension: "momd") else {
+            fatalError("Failed to locate model file.")
+        }
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: url) else {
+            fatalError("Failed to load model file.")
+        }
+        return managedObjectModel
+    }()
+
+    func save() {
+        if container.viewContext.hasChanges {
+            try? container.viewContext.save()
+        }
+    }
+
+    func delete(_ object: NSManagedObject) {
+        container.viewContext.delete(object)
+    }
+
+    func deleteAll() {
+        // Create fetch request
+        // Create batch delete request
+        // execute batch
+    }
+
+    func count<T>(for fetchRequest: NSFetchRequest<T>) -> Int {
+        (try? container.viewContext.count(for: fetchRequest)) ?? 0
+    }
+}
